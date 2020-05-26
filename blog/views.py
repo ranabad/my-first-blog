5c23a396a6fa3b2240e4cc3d5b5272091ca1a539
ranaalbadrani <rana.a.albadrani@gmail.com>
2020-05-26 15:46:07 +0300

    Changed blog/views again  but still have issues with Post.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
+# Create your views here.
 from django.shortcuts import render
-from .models import Post
 from django.utils import timezone
+from .models import Post
 
 def post_list(request):
+    # doc strings? here.
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
