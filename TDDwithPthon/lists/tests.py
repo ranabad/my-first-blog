a3d2398a1ef1870838942af089b894d06bb6812d
ranaalbadrani <rana.a.albadrani@gmail.com>
2020-06-02 16:15:48 +0300

    corrected tests.py

--- TDDwithPthon/lists/tests.py
+++ TDDwithPthon/lists/tests.py
@@ -1,24 +1,8 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
 
- 
-
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-
-
-    def test_home_page_returns_correct_html(self): 
-        response = self.client.get('/')  
-
-        html = response.content.decode('utf8')  
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-
-        self.assertTemplateUsed(response, 'home.html')  
