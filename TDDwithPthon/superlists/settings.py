12c73ca680d8248ae21828c7e9d97e4a1a1e84f6
ranaalbadrani <rana.a.albadrani@gmail.com>
2020-06-08 03:51:38 +0300

    worked with static dirctory uses collect static

--- TDDwithPthon/superlists/settings.py
+++ TDDwithPthon/superlists/settings.py
@@ -28,7 +28,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -116,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
