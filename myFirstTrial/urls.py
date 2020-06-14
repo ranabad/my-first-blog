a7b129a6f64982fdbec77ddce2520bdda0675174
ranaalbadrani <rana.a.albadrani@gmail.com>
2020-06-14 21:50:52 +0300

    changed template structure

--- myFirstTrial/urls.py
+++ myFirstTrial/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path, include
 from django.contrib import admin
-
 from django.contrib.auth import views
+from django.urls import include, path
 
 urlpatterns = [
    path('admin/', admin.site.urls),
