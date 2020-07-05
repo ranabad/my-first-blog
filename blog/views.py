f5783c6e043601f520debcf89e78e29d987258cc
ranaalbadrani <rana.a.albadrani@gmail.com>
2020-07-05 16:54:50 +0300

    why

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ from .models import Comment, Post
 
 def InteractiveCV(request):
 
-    return render(request, 'blog/InteractiveCv/cv.html')
+    return render(request, '../InteractiveCv/cv.html')
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
