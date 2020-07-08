8aaa4093833d5cc8a359be498e972f4314adc1fc
ranaalbadrani <rana.a.albadrani@gmail.com>
2020-07-08 03:49:01 +0300

    working on FT

--- blog/models.py
+++ blog/models.py
@@ -40,7 +40,5 @@ class Comment(models.Model):
 class Item(models.Model):
     text = models.CharField(max_length=200)
     created_date = models.DateTimeField(default=timezone.now)
-    def add(self):
-        self.save()
     def __str__(self):
         return self.text
