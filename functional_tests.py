d69c0fd90423693b0fa10fec2e7684d3920506b3
ranaalbadrani <rana.a.albadrani@gmail.com>
2020-07-11 02:04:30 +0300

    FT and UnitTest

--- functional_tests.py
+++ functional_tests.py
@@ -1,48 +1,43 @@
-""" import time
-import unittest
-
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
+import unittest
 
 class NewVisitorTest(unittest.TestCase):  
 
     def setUp(self):  
-        self.browser = webdriver.Safari()
+        self.browser = webdriver.Firefox()
 
     def tearDown(self):  
         self.browser.quit()
 
-    def test_home_page(self):    
-        self.browser.get('http://ranabad.pythonanywhere.com')
-        self.assertIn('Personal Blog', self.browser.title) 
-           
-    def test_can_get_cv_page(self):  
-        self.browser.get('http://ranabad.pythonanywhere.com/cv')
+    def test_can_start_a_list_and_retrieve_it_later(self):  
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://127.0.0.1:8000/cv')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('Personal Blog', self.browser.title)  
+
+
+
+        header_text = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn("Rana's Personal Blog", header_text)
+
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_tag_name('h3').text 
+        self.assertIn("Education", inputbox)
+
         
 
-        inputbox = self.browser.find_element_by_id('id_new_item')  
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a new skill'
-        )
-
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
         
+
+
+
+
+         
 
        
-        inputbox.send_keys('django')  
-        inputbox.send_keys(Keys.ENTER)  
-        time.sleep(1)  
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')  
-        self.assertTrue(
-    any(row.text == '1: django' for row in rows),
-    f"New to-do item did not appear in table. Contents were:\n{table.text}"  
-)
-
-
-        self.fail('Finish the test!')
-
-        # The page updates again, and now shows both items on her list
- """
+if __name__ == '__main__':  
+    unittest.main(warnings='ignore')
