f9c5c16510f584ef5c834c73a309c75d03732724
ranaalbadrani <rana.a.albadrani@gmail.com>
2020-07-18 07:55:49 +0300

    did CV, some CSS and psql

--- myFirstTrial/settings.py
+++ myFirstTrial/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': 'djangodata',
         'USER': 'ranabad',
         'PASSWORD': '',
-        'HOST': 'localhost','.pythonanywhere.com',
+        'HOST': 'localhost','.pythonanywhere.com'
         'PORT': '',
     
 }
