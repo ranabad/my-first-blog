6417f32a88748ba1a67c314a1e99312a31d0d81c
ranaalbadrani <rana.a.albadrani@gmail.com>
2020-07-18 13:24:10 +0300

    trying some tricks for security

--- myFirstTrial/wsgi.py
+++ myFirstTrial/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myFirstTrial.settings')
 
+# assuming your django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/ranabad/ranabad.pythonanywhere.com'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myFirstTrial.settings'
+
+# serve django via WSGI
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
