f90f8a3a0e78470ced3b83f82ece50bea64f8c94
ranaalbadrani <rana.a.albadrani@gmail.com>
2020-07-19 15:23:33 +0300

    imported contact to form

--- myFirstTrial/settings.py
+++ myFirstTrial/settings.py
@@ -156,5 +156,4 @@ LOGIN_REDIRECT_URL = '/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-DESKTOP_TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'blog'),)
-MOBILE_TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'blog/mobile'),)
+
