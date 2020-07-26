40a7ef747930ff51da15bf5c06f2b9a4dd3a4524
ranabad <rana.a.albadrani@gmail.com>
2020-07-27 00:17:47 +0300

    fixing logig in unit test

--- blog/tests.py
+++ blog/tests.py
@@ -32,6 +32,7 @@ class CVEducationTest(TestCase):
          print(posting.content)
          editing=c.get('/cv/<1:pk>/Education/edit/')
          EducationForm.clean
+         print(response.content)
          print("-------------------------------------------------------------------")
          editing=c.post('/cv/<1:pk>/Education/edit/',{'text':'abc', 'date':'1'})
          editing.status_code
