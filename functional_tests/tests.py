e72f7e15a4d94717ed919ecc905bf32277ae62f2
ranabad <rana.a.albadrani@gmail.com>
2020-07-26 10:34:49 +0300

    some changes to FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,7 +5,8 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 
 from pyvirtualdisplay import Display
-with Display():
+
+
 display = Display(visible=0, size=(800, 600))
 display.start()
 browser = webdriver.Firefox()
