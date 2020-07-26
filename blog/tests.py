0eb16b6eb0252bb0571767a73d0e0a708a9fc11f
ranabad <rana.a.albadrani@gmail.com>
2020-07-26 18:12:07 +0300

    fixing logig in unit test

--- blog/tests.py
+++ blog/tests.py
@@ -30,9 +30,8 @@ class CVEducationTest(TestCase):
          self.assertEqual(Education.objects.count(),1)
          posting=c.get('/cv')
          print(posting.content)
-         pk=Education.objects.get().id
-         editing=c.get('/cv/{pk}/Education/edit/')
-         editing=c.post('/cv/{pk}/Education/edit/',{'text':'abc', 'date':'1'})
+         editing=c.get('/cv/1/Education/edit/')
+         editing=c.post('/cv/1/Education/edit/',{'text':'abc', 'date':'1'})
          editing.status_code
          self.assertEqual(Education.objects.count(),1)
          editing=c.get('/cv')
