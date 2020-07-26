85d967e519bf327e81bc2506d222efbe6451156a
ranabad <rana.a.albadrani@gmail.com>
2020-07-26 09:36:20 +0300

    some changes to FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,23 +3,28 @@ import time
 from django.test import LiveServerTestCase
 from selenium import webdriver
 
+from pyvirtualdisplay import Display
 
-class NewVisitorTest(LiveServerTestCase):  
-
-    def setUp(self):  
-        self.browser = webdriver.Safari
-
-    def tearDown(self):  
-        self.browser.quit()
-
-    def test_can_start_a_list_page_and_retrieve(self):
-        #I wont test whether Post Method works because USER MUST BE ADMIN (ME)
-        #so functional test shows that browser can load up cv page 
-        # it prints out for vistors that are not the admin(ME) the basic html page and what contains to make sure they can see CV items
-        self.browser.get(self.live_server_url)
-        self.assertIn('Personal Blog', self.browser.title)  
-        html = self.browser.page_source
+display = Display(visible=0, size=(800, 600))
+display.start()
+try:
+   
+    browser = webdriver.Firefox()
+    browser.get('http://www.ranabad.pythonanywhere.com')
+  
+    assertIn('Personal Blog', browser.title)  
+        html = browser.page_source
         time.sleep(10)
         print(html)
 
+finally:
+    
+    browser.quit()
+    display.stop() 
+
+       
+
         
+        
+
+
