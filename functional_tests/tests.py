5feb4b91a602bd40421ff22976e05b3c849852ef
ranabad <rana.a.albadrani@gmail.com>
2020-07-26 10:59:18 +0300

    added comments to the final version of FT and now it can run on pythonanywhere

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,12 @@
+"""I wont test whether Post Method works because USER MUST BE ADMIN (ME)
+  
+    so functional test shows that browser can load up cv page 
+
+    it prints out for vistors that are not the admin(ME) the basic html 
+    
+    page and what contains to make sure they can see CV items 
+
+    I test the functional test with my pythonpythonanywhere console"""  
 import time
 import unittest
 
