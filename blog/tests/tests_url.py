5c42ae3b0ea52e15a479660085e9b2705de55813
ranabad <rana.a.albadrani@gmail.com>
2020-07-27 18:03:50 +0300

    fixing logig in unit test

--- blog/tests/tests_url.py
+++ blog/tests/tests_url.py
@@ -1,6 +1,10 @@
 from django.test import SimpleTestCase
+from danjgo.urls import reverse,resolve
+from blog.views import EducationCV
 
 
 class TestUrls(SimpleTestCase):
     def test_Education_url_is_resolved(self):
-        assert 1==2
+        path('cv/Education', views.EducationCV, name='cvEducation'),
+        url=reverse('cvEducation')
+        print(resolve(url))
