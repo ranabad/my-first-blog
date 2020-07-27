5d96c145b336f7cb1582edb777baf1dc7347c31e
ranabad <rana.a.albadrani@gmail.com>
2020-07-27 19:35:19 +0300

    fixing logig in unit test

--- blog/tests/tests_views.py
+++ blog/tests/tests_views.py
@@ -9,14 +9,14 @@ class TestViews(TestCase):
         self.client=Client()
         self.url_cv=reverse('cv')
         self.url_edu=reverse('cvEducation')
+        self.edu=Education.objects.create(text='abc',date='123')
     def test_CV_contents_GET(self):
         response=self.client.get(self.url_cv)
         self.assertEquals(response.status_code,200)
         self.assertTemplateUsed(response,'blog/cv.html')
     def test_CV_contents_with_POST(self):
-        edu=Education.objects.create(text='abc',date='123')
         response=self.client.post(self.url_edu,{'text':'abc','date':'123'})
         self.assertEquals(response.status_code,302)
-        self.assertEquals(edu.first().text,'abc')
+        self.assertEquals(self.edu.first().text,'abc')
 
         
