8e64dc1c16f979cb1804e1d32fdf9467f99b17cc
ranabad <rana.a.albadrani@gmail.com>
2020-07-27 18:51:19 +0300

    fixing logig in unit test

--- blog/tests/tests_url.py
+++ blog/tests/tests_url.py
@@ -1,6 +1,11 @@
 from django.test import SimpleTestCase
-from django.urls import reverse,resolve
-from blog.views import EducationCV,CV,SkillsCV,WorkshopsCV,ExperienceCV,ContactCV,ContactCVMsg,SkillsCVUpdatde
+from django.urls import resolve, reverse
+
+from blog.views import (CV, ContactCV, ContactCVMsg, EducationCV,
+                        EducationCVDlt, EducationCVUpdatde, ExperienceCV,
+                        ExperienceCVDlt, ExperienceCVUpdatde, SkillsCV,
+                        SkillsCVDlt, SkillsCVUpdatde, WorkshopsCV,
+                        WorkshopsCVDlt, WorkshopsCVUpdatde, msg_remove)
 
 
 class TestUrls(SimpleTestCase):
@@ -21,13 +26,26 @@ class TestUrls(SimpleTestCase):
         self.assertEquals(resolve(url_workshop).func,WorkshopsCV)
     def test_edu_skills_workshop_exp_update_url_is_resolved(self):
         url_skill=reverse('cv_Skills_edit',args=[1])
+        url_workshop=reverse('cv_Workshop_edit',args=[1])
+        url_exp=reverse('cv_Experience_edit',args=[1])
+        url_edu=reverse('cv_Education_edit',args=[1])
         self.assertEquals(resolve(url_skill).func,SkillsCVUpdatde)
+        self.assertEquals(resolve(url_workshop).func,WorkshopsCVUpdatde)
+        self.assertEquals(resolve(url_exp).func,ExperienceCVUpdatde)
+        self.assertEquals(resolve(url_edu).func,EducationCVUpdatde)
+    def test_edu_skills_workshop_exp_msg_dlt_url_is_resolved(self):
+        url_skill=reverse('cv_Skills_dlt',args=[1])
+        url_workshop=reverse('cv_Workshop_dlt',args=[1])
+        url_exp=reverse('cv_Experience_dlt',args=[1])
+        url_edu=reverse('cv_Education_dlt',args=[1])
+        url_msg=reverse('msg_remove',args=[1])
+        self.assertEquals(resolve(url_msg).func,msg_remove)
+        self.assertEquals(resolve(url_skill).func,SkillsCVDlt)
+        self.assertEquals(resolve(url_workshop).func,WorkshopsCVDlt)
+        self.assertEquals(resolve(url_exp).func,ExperienceCVDlt)
+        self.assertEquals(resolve(url_edu).func,EducationCVDlt)
+           
+ 
+ 
 
-
-        
-
-    
-
-          
-    
-    
+   
