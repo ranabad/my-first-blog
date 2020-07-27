4a014e3ef795dd9f70305eb9e5f8a6549c832f93
ranabad <rana.a.albadrani@gmail.com>
2020-07-27 19:50:57 +0300

    fixing logig in unit test

--- blog/tests/tests_views.py
+++ blog/tests/tests_views.py
@@ -6,16 +6,22 @@ import json
 
 class TestViews(TestCase):
     def setUp(self):
-        self.client=Client()
+    
+        user = User.objects.create(username='testuser')
+        user.set_password('12345')
+        user.save()
+        self.c = Client()
+        logged_in = c.login(username='testuser', password='12345')
+        self.assertTrue(logged_in) 
         self.url_cv=reverse('cv')
         self.url_edu=reverse('cvEducation')
         self.edu=Education.objects.create(text='abc',date='123')
     def test_CV_contents_GET(self):
-        response=self.client.get(self.url_cv)
+        response=self.c.get(self.url_cv)
         self.assertEquals(response.status_code,200)
         self.assertTemplateUsed(response,'blog/cv.html')
     def test_CV_contents_with_POST(self):
-        response=self.client.post(self.url_edu,{'text':'abc','date':'123'})
+        response=self.c.post(self.url_edu,{'text':'abc','date':'123'})
         self.assertEquals(response.status_code,302)
         #self.assertEquals(self.edu.first().text,'abc')
         #self.assertEqual(Education.objects.count(),1)
