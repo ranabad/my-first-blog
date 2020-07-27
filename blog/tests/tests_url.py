d99ceb9fe12bab1e9f0cc9e8f905c0e9fc09c8de
ranabad <rana.a.albadrani@gmail.com>
2020-07-27 18:06:22 +0300

    fixing logig in unit test

--- blog/tests/tests_url.py
+++ blog/tests/tests_url.py
@@ -5,6 +5,5 @@ from blog.views import EducationCV
 
 class TestUrls(SimpleTestCase):
     def test_Education_url_is_resolved(self):
-        path('cv/Education', views.EducationCV, name='cvEducation'),
         url=reverse('cvEducation')
         print(resolve(url))
