2b8ca5940628783573f2585be5ec990e25f83723
ranabad <rana.a.albadrani@gmail.com>
2020-07-27 12:04:16 +0300

    fixing logig in unit test

--- blog/tests.py
+++ blog/tests.py
@@ -31,7 +31,6 @@ class CVEducationTest(TestCase):
          posting=c.get('/cv')
          print(posting.content)
          new_club = Education.objects.create(text="hi",date="hello")
-         self.assertEqual(Education[1].id, 1)
          G=c.put("cv/{}/Education/edit/".format(new_club.pk),{'text':'abc', 'date':'FAIL'})
          new_club.refresh_from_db()
          G.status_code
