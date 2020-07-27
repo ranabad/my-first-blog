71af5c7cecfc7733c088d1656d43ad638d62ffb9
ranabad <rana.a.albadrani@gmail.com>
2020-07-27 18:23:00 +0300

    fixing logig in unit test

--- blog/tests/tests_url.py
+++ blog/tests/tests_url.py
@@ -1,10 +1,27 @@
 from django.test import SimpleTestCase
 from django.urls import reverse,resolve
-from blog.views import EducationCV
+from blog.views import EducationCV,CV,SkillsCV,WorkshopsCV,ExperienceCV,ContactCV,ContactCVMsg
 
 
 class TestUrls(SimpleTestCase):
-    def test_Education_url_is_resolved(self):
-        url=reverse('cvEducation')
-        print(resolve(url))
-        self.assertEquals(resolve(url).func,EducationCV)
+    def test_cv_edu_skills_workshop_exp_url_is_resolved(self):
+        url_skill=reverse('cvSkills')
+        url_main=reverse('cv')
+        url_workshop=reverse('cvWorkshop')
+        url_exp=reverse('cvExperience')
+        url_edu=reverse('cvEducation')
+        url_con=reverse('contact')
+        url_msg=reverse('msg')
+        self.assertEquals(resolve(url_edu).func,EducationCV)
+        self.assertEquals(resolve(url_con).func,ContactCV)
+        self.assertEquals(resolve(url_msg).func,ContactCVMsg)
+        self.assertEquals(resolve(url_exp).func,ExperienceCV)
+        self.assertEquals(resolve(url_main).func,CV)
+        self.assertEquals(resolve(url_skill).func,SkillsCV)
+        self.assertEquals(resolve(url_workshop).func,WorkshopsCV)
+
+          
+    
+    
+    
+  
