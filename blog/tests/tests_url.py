fc35e2c3621c31c3b256fdf8b91597097f996d26
ranabad <rana.a.albadrani@gmail.com>
2020-07-27 18:41:46 +0300

    fixing logig in unit test

--- blog/tests/tests_url.py
+++ blog/tests/tests_url.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse,resolve
-from blog.views import EducationCV,CV,SkillsCV,WorkshopsCV,ExperienceCV,ContactCV,ContactCVMsg
+from blog.views import EducationCV,CV,SkillsCV,WorkshopsCV,ExperienceCV,ContactCV,ContactCVMsg,SkillsCVUpdatde
 
 
 class TestUrls(SimpleTestCase):
