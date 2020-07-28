7528fd0d25c444225335c3a206d2e0fdd901208f
ranabad <rana.a.albadrani@gmail.com>
2020-07-28 13:56:36 +0300

    fixing logig in unit test

--- blog/tests/tests_views.py
+++ blog/tests/tests_views.py
@@ -9,25 +9,24 @@ from blog.models import Contact, Education, Experience, Skills, Workshops
 
 class TestViews(TestCase):
     def setUp(self):
-    
         user = User.objects.create(username='testuser')
         user.set_password('12345')
         user.save()
-        self.c = Client()
-        logged_in =self.c.login(username='testuser', password='12345')
+        c = Client()
+        logged_in =c.login(username='testuser', password='12345')
         self.assertTrue(logged_in) 
         self.url_cv=reverse('cv')
         self.url_edu=reverse('cvEducation')  
     def test_CV_contents_GET(self):
-        response=self.c.get(self.url_cv)
+        response=c.get(self.url_cv)
         self.assertEquals(response.status_code,200)
         self.assertTemplateUsed(response,'blog/cv.html')
     def test_CV_contents_with_POST(self):
         Education.objects.create(text='itemey 1',date='2020')
-        response = self.c.post(self.url_edu,data={'text':'abc','date':'123'}
-        #self.assertEqual(response.status_code, 302)
-        #self.assertTemplateUsed(response,'blog/cv/cvEducation.html')
-        #self.assertEquals(Education.objects.count(),1)
+        response = c.post(self.url_edu,data={'text':'abc','date':'123'}
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed(response,'blog/cv/cvEducation.html')
+        self.assertEquals(Education.objects.count(),1)
         print(response.content)
 
 
