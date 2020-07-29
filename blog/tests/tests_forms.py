3e3b4421ccda3c77c6d63f284821e18c08df71ce
ranabad <rana.a.albadrani@gmail.com>
2020-07-29 15:06:32 +0300

    fixing logig in unit test

--- blog/tests/tests_forms.py
+++ blog/tests/tests_forms.py
@@ -9,5 +9,11 @@ class testForms(SimpleTestCase):
     def test_form_valid_data(self):
         form=ContactForm(data={'from_email':'123@gmail.com','from_name':'tester','subject':'modeltest','message':'testing forms now'})
         self.assertTrue(form.is_valid())
+        form=EducationForm(data={'text':'abc','date':'123'})
+        self.assertTrue(form.is_valid())
+        form=ExperienceForm(data={'text':'abc','date':'123'})
+        self.assertTrue(form.is_valid())
+        form=WorkshopsForm(data={'text':'abc','date':'123'})
+        self.assertTrue(form.is_valid())
         form=EducationForm(data={'text':'abc'})
         self.assertTrue(form.is_valid())
