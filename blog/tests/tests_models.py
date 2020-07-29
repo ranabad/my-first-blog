55e5aba4a4c30301a79026e23bd076dfcd599264
ranabad <rana.a.albadrani@gmail.com>
2020-07-29 14:23:40 +0300

    fixing logig in unit test

--- blog/tests/tests_models.py
+++ blog/tests/tests_models.py
@@ -18,4 +18,33 @@ class ModelTest(TestCase):
          first_saved_item = saved_items[0]
          second_saved_item = saved_items[1]
          self.assertEqual(first_saved_item.text, 'abc')
-         self.assertEqual(second_saved_item.text, 'def') 
+         self.assertEqual(second_saved_item.text, 'def')
+         first_item = Skills()
+         first_item.text = 'ghi'
+         first_item.save()
+         second_item = Skills()
+         second_item.text = 'jkl'
+         second_item.save()
+         saved_items = Skills.objects.all()
+         self.assertEqual(saved_items.count(), 2)
+         first_saved_item = saved_items[0]
+         second_saved_item = saved_items[1]
+         self.assertEqual(first_saved_item.text, 'ghi)
+         self.assertEqual(second_saved_item.text, 'jkl')
+         first_item = Experience()
+         first_item.text = 'mno'
+         first_item.save()
+         second_item = Experience()
+         second_item.text = 'pqr'
+         second_item.save()
+         saved_items = Experience.objects.all()
+         self.assertEqual(saved_items.count(), 2)
+         first_saved_item = saved_items[0]
+         second_saved_item = saved_items[1]
+         self.assertEqual(first_saved_item.text, 'mno')
+         self.assertEqual(second_saved_item.text, 'pqr')
+         self.assertEquals(Experience.objects.first().date,'Present')
+
+
+
+
