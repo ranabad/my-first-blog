35f2959fe684389e854a82b69457b5c022d9acb6
ranabad <rana.a.albadrani@gmail.com>
2020-07-29 14:27:27 +0300

    fixing logig in unit test

--- blog/tests/tests_models.py
+++ blog/tests/tests_models.py
@@ -31,6 +31,7 @@ class ModelTest(TestCase):
          second_saved_item = saved_items[1]
          self.assertEqual(first_saved_item.text, 'ghi')
          self.assertEqual(second_saved_item.text, 'jkl')
+         self.assertEquals(Experience.objects.first().date,'Present')
          first_item = Experience()
          first_item.text = 'mno'
          first_item.save()
