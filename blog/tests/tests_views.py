9d95494a88d23285a8d625343e3f444b9e538c8f
ranabad <rana.a.albadrani@gmail.com>
2020-08-02 00:57:58 +0300

    trying to test cv key

--- blog/tests/tests_views.py
+++ blog/tests/tests_views.py
@@ -82,9 +82,8 @@ class TestViews(TestCase):
         self.assertIn('tester', response.content.decode())
     def test_CV_contents_with_Update_POST(self):
         Education.objects.create(text='abc',date='123')
-        req=self.c.get(url_edu)
-        posting=self.c.post(req,data={'text':'abc','date':'123'})
-        posting = EducationCVUpdatde(self.c.url_edu, pk=1)
+        posting=self.c.post(self.url_edu,data={'text':'abc','date':'123'})
+        posting = EducationCVUpdatde(self.c.get(url_eduUp), pk=1)
         posting=self.post(posting,data={'text':'cab','date':'321'})
         assertEquals(posting.status_code,302)
         assertEqual(Education.objects.count(),1)
